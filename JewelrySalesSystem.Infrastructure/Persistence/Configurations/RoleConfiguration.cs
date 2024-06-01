7b1c0bd4341114b7ee8b22df85147a35dbbb5e10
iterctink <ducanhle.iter@gmail.com>
2024-06-01 10:15:31 +0700

    Added: RoleConfiguration, CounterConfiguration. Update: UserConfiguration

---
+++ JewelrySalesSystem.Infrastructure/Persistence/Configurations/RoleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelrySalesSystem.Infrastructure.Persistence.Configurations
+{
+    internal class RoleConfiguration
+    {
+    }
+}
