32dc8de7fd36e691961da43728221907591a4b3b
NemissU <namphse171651@fpt.edu.vn>
2024-06-26 10:58:59 +0700

    Nam: Add FindAllByAsync, FindAllPaginationAsync, FindAllPaginationByAsync, GetRoleByPagination API, Configured PagedResult of Pagination

--- Jewelry_Sales_System/Controllers/RoleController.cs
+++ Jewelry_Sales_System/Controllers/RoleController.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using JewelrySalesSystem.Application.Common.Pagination;
+using JewelrySalesSystem.Application.Role;
 using JewelrySalesSystem.Application.Role.CreateRole;
+using JewelrySalesSystem.Application.Role.GetByPagination;
 using JewelrySalesSystem.Application.Users.CreateNewUser;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -19,7 +22,7 @@ namespace Jewelry_Sales_System.API.Controllers
         }
 
         [HttpPost]
-        [Route("create")]
+        [Route("role/create")]
         [Authorize(Roles = "Admin")]
         [Produces(MediaTypeNames.Application.Json)]
         [ProducesResponseType(typeof(JsonResponse<string>), StatusCodes.Status201Created)]
@@ -31,6 +34,21 @@ namespace Jewelry_Sales_System.API.Controllers
         {
             var result = await _mediator.Send(command, cancellationToken);
             return Ok(new JsonResponse<string>(result));
+        }
+
+        [HttpGet("role/pagination")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<PagedResult<RoleDto>>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(JsonResponse<PagedResult<RoleDto>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<PagedResult<RoleDto>>>> GetPagination([FromQuery] GetRoleByPaginationQuery query
+            , CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(query, cancellationToken);
+            return Ok(result);
         }
     }
 }
