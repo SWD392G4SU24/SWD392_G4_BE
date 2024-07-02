215de1dd60ea382f911c677114e4800768b2a9d3
duc-anh-le <ducanhle.iter@gmail.com>
2024-07-02 18:50:39 +0700

    Anh: Login via Google mail

---
+++ JewelrySalesSystem.Application/Users/GoogleLogin/SignInCallback/SignInGoogleCallbackQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+
+namespace JewelrySalesSystem.Application.GoogleLogin.SignInCallback
+{
+    public class SignInGoogleCallbackQuery : IRequest<SignInGoogleCallbackResponse>
+{
+        public HttpContext HttpContext { get; }
+
+        public SignInGoogleCallbackQuery(HttpContext httpContext)
+    {
+            HttpContext = httpContext;
+        }
+    }
+}
---
+++ JewelrySalesSystem.Application/Users/GoogleLogin/SignInCallback/SignInGoogleCallbackResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace JewelrySalesSystem.Application.GoogleLogin.SignInCallback
+{
+    public class SignInGoogleCallbackResponse
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Token { get; set; }
+
+namespace JewelrySalesSystem.Application.Users.GoogleLogin.SignInCallback
+{
+    internal class SignInGoogleCallbackResponse
+    {
+    }
+}
