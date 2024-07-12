b84e99fa9aa35706bb8101b61328b045ca1f02bb
NemissU <namphse171651@fpt.edu.vn>
2024-07-12 09:04:33 +0700

    Nam: fixed API CreateOrderByStaff, UpdateOrder, add FindAll queryOptions, add FilterUser API

---
+++ JewelrySalesSystem.Application/Users/FilterUser/FilterUserQuery.cs
@@ -0,0 +1,31 @@
+﻿using JewelrySalesSystem.Application.Common.Interfaces;
+using JewelrySalesSystem.Application.Common.Pagination;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelrySalesSystem.Application.Users.FilterUser
+{
+    public class FilterUserQuery : IRequest<PagedResult<UserDto>>, IQuery
+    {
+        public FilterUserQuery()
+        {
+            
+        }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string? FullName {  get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public int? RoleID { get; set; } = 0;
+        public FilterUserQuery(int no, int pageSize)
+        {
+            PageNumber = no;
+            PageSize = pageSize;
+        }
+    }
+}
