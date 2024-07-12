ca8575e63eb88ba1f974164a783b49f92fa4c18e
duc-anh-le <ducanhle.iter@gmail.com>
2024-07-12 18:13:32 +0700

    Anh: Add filter for category

---
+++ JewelrySalesSystem.Application/Category/FilterCategory/FilterCategoryQuery.cs
@@ -0,0 +1,24 @@
+﻿using JewelrySalesSystem.Application.Common.Interfaces;
+using JewelrySalesSystem.Application.Common.Pagination;
+using MediatR;
+
+namespace JewelrySalesSystem.Application.Category.FilterCategory
+{
+    public class FilterCategoryQuery : IRequest<PagedResult<CategoryDto>>, IQuery
+    {
+        public FilterCategoryQuery()
+        {
+
+        }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string? Name { get; set; }
+
+        public FilterCategoryQuery(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
---
+++ JewelrySalesSystem.Application/Category/FilterCategory/FilterCategoryQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using JewelrySalesSystem.Application.Common.Pagination;
+using JewelrySalesSystem.Domain.Commons.Exceptions;
+using JewelrySalesSystem.Domain.Entities.Configured;
+using JewelrySalesSystem.Domain.Repositories.ConfiguredEntity;
+using MediatR;
+
+namespace JewelrySalesSystem.Application.Category.FilterCategory
+{
+    public class FilterCategoryQueryHandler : IRequestHandler<FilterCategoryQuery, PagedResult<CategoryDto>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public FilterCategoryQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<PagedResult<CategoryDto>> Handle(FilterCategoryQuery request, CancellationToken cancellationToken)
+        {
+            Func<IQueryable<CategoryEntity>, IQueryable<CategoryEntity>> queryOptions = query =>
+            {
+                query = query.Where(x => x.DeletedAt == null);
+
+                if (!string.IsNullOrEmpty(request.Name))
+                {
+                    query = query.Where(x => x.Name.Contains(request.Name));
+                }
+
+                return query;
+            };
+
+            var result = await _categoryRepository.FindAllAsync(request.PageNumber, request.PageSize, queryOptions, cancellationToken);
+            if (!result.Any())
+            {
+                throw new NotFoundException("Không tìm thấy Category");
+            }
+
+            return PagedResult<CategoryDto>.Create(
+                totalCount: result.TotalCount,
+                pageCount: result.PageCount,
+                pageSize: result.PageSize,
+                pageNumber: result.PageNo,
+                data: result.MapToCategoryDtoList(_mapper));
+        }
+    }
+}
