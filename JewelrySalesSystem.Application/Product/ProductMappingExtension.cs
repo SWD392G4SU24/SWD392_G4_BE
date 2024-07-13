0cfa019a9347b7f719c3e3f8b3f931a7b940f94d
NemissU <namphse171651@fpt.edu.vn>
2024-07-13 22:12:41 +0700

    Nam: add ProductCost response DTO

--- JewelrySalesSystem.Application/Product/ProductMappingExtension.cs
+++ JewelrySalesSystem.Application/Product/ProductMappingExtension.cs
@@ -11,21 +11,23 @@ namespace JewelrySalesSystem.Application.Product
         public static List<ProductDto> MapToProductDtoList(this IEnumerable<ProductEntity> projectFrom, IMapper mapper)
             => projectFrom.Select(x => x.MapToProductDto(mapper)).ToList();
 
-        public static ProductDto MapToProductDto(this ProductEntity entity, IMapper mapper, string? goldType, string? diamondType, string category)
+        public static ProductDto MapToProductDto(this ProductEntity entity, IMapper mapper, string? goldType, string? diamondType, string category, decimal productCost)
         {
             var dto = mapper.Map<ProductDto>(entity);
             dto.GoldType = goldType;
             dto.DiamondType = diamondType;
             dto.Category = category;
+            dto.ProductCost = productCost;
             return dto;
         }
 
-        public static List<ProductDto> MapToProductDtoList(this IEnumerable<ProductEntity> entities, IMapper mapper, Dictionary<int, string>? goldType, Dictionary<int, string>? diamondType, Dictionary<int, string> category)
+        public static List<ProductDto> MapToProductDtoList(this IEnumerable<ProductEntity> entities, IMapper mapper, Dictionary<int, string>? goldType, Dictionary<int, string>? diamondType, Dictionary<int, string> category, Dictionary<string, decimal> productCost)
             => entities.Select(x =>
             x.MapToProductDto(mapper,
                 x.GoldID.HasValue && goldType != null && goldType.ContainsKey(x.GoldID.Value) ? goldType[x.GoldID.Value] : null,
                 x.DiamondID.HasValue && diamondType != null && diamondType.ContainsKey(x.DiamondID.Value) ? diamondType[x.DiamondID.Value] : null,
-                category.ContainsKey(x.CategoryID) ? category[x.CategoryID] : "Lỗi"
+                category.ContainsKey(x.CategoryID) ? category[x.CategoryID] : "Lỗi",
+                productCost.ContainsKey(x.ID) ? productCost[x.ID] : 0
             )).ToList();
 
     }
