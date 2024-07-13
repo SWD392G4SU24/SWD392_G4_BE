8f76cf09333e314b2d7785dbb9b67d1c56d5bf09
Bi.nhat.goose <binhatgoose0410@gmail.com>
2024-07-13 19:25:45 +0700

    MNhat: Push code API R(Promotion- updateUserId)

---
+++ JewelrySalesSystem.Application/Promotion/ExchangePoint/UpdateUserIDByPromotionCommand.cs
@@ -0,0 +1,26 @@
+﻿using JewelrySalesSystem.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelrySalesSystem.Application.Promotion.ExchangePoint
+{
+    public class UpdateUserIDByPromotionCommand : IRequest<string>, ICommand
+    {
+        public UpdateUserIDByPromotionCommand(string customerID, string orderID, string voucherCode)
+        {
+            CustomerID = customerID;
+            OrderID = orderID;
+            VoucherCode = voucherCode;
+        }
+
+        public string CustomerID { get; set; }
+        public string OrderID { get; set; }
+        public string VoucherCode { get; set; }
+        
+    }
+
+}
