12f96232c53fd2bdeb84a6006faeb409bc819450
duc-anh-le <ducanhle.iter@gmail.com>
2024-07-14 14:29:45 +0700

    Anh: Add get by category for counter

--- Jewelry_Sales_System/Controllers/CounterController.cs
+++ Jewelry_Sales_System/Controllers/CounterController.cs
@@ -5,6 +5,7 @@ using JewelrySalesSystem.Application.Counter.Delete;
 using JewelrySalesSystem.Application.Counter.FilterCounter;
 using JewelrySalesSystem.Application.Counter.FilterRevenue;
 using JewelrySalesSystem.Application.Counter.GetAll;
+using JewelrySalesSystem.Application.Counter.GetByCategory;
 using JewelrySalesSystem.Application.Counter.GetById;
 using JewelrySalesSystem.Application.Counter.GetByPagination;
 using JewelrySalesSystem.Application.Counter.Update;
@@ -86,6 +87,20 @@ namespace Jewelry_Sales_System.API.Controllers
             var result = await _mediator.Send(new GetCounterByIdQuery(id: id), cancellationToken);
             return result != null ? Ok(new JsonResponse<CounterDto>(result)) : NotFound();
         }
+
+        [HttpGet("counter/by-category")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<PagedResult<CounterDto>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<PagedResult<CounterDto>>>> GetCountersByCategory([FromQuery] GetCountersByCategoryQuery query, CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(query, cancellationToken);
+            return Ok(result);
+        }
+
 
         [HttpPut("counter/update")]
         [Produces(MediaTypeNames.Application.Json)]
