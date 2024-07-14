9b433c437151870bebf913cd092f1e73d349e922
NemissU <namphse171651@fpt.edu.vn>
2024-07-14 17:04:18 +0700

    Nam: Staff revenue API

---
+++ JewelrySalesSystem.Application/Users/StaffRevenue/FilterStaffRevenueQueryHandler.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using JewelrySalesSystem.Domain.Repositories.ConfiguredEntity;
+using JewelrySalesSystem.Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JewelrySalesSystem.Domain.Entities.Configured;
+using static JewelrySalesSystem.Domain.Commons.Enums.Enums;
+using JewelrySalesSystem.Domain.Entities;
+using JewelrySalesSystem.Infrastructure.Repositories.ConfiguredEntity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JewelrySalesSystem.Application.Users.StaffRevenue
+{
+    public class FilterStaffRevenueQueryHandler : IRequestHandler<FilterStaffRevenueQuery, List<StaffRevenueDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+        private readonly IOrderRepository _orderRepository;
+        public FilterStaffRevenueQueryHandler(IMapper mapper
+            , IUserRepository userRepository
+            , IOrderRepository orderRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+            _orderRepository = orderRepository;
+        }
+        public async Task<List<StaffRevenueDto>> Handle(FilterStaffRevenueQuery request, CancellationToken cancellationToken)
+        {
+            Func<IQueryable<UserEntity>, IQueryable<UserEntity>> queryoptions = query =>
+            {
+                if (!string.IsNullOrEmpty(request.StaffID))
+                {
+                    query = query.Where(x => x.ID == request.StaffID);
+                }
+                query = query.Where(x => x.Orders.Any(y => y.CreatedAt <= request.EndDate
+                    && y.CreatedAt >= request.StartDate
+                    && y.DeletedAt == null
+                    && (y.Status.Equals(OrderStatus.COMPLETED))
+                    ));
+                return query;
+            };
+            var result = await _userRepository.FindAllAsync(x => x.DeletedAt == null, queryoptions, cancellationToken);
+
+            Dictionary<string, decimal> total = new Dictionary<string, decimal>();
+            Dictionary<string, int> ordersCount = new Dictionary<string, int>();
+            var orders = await _orderRepository.FindAllAsync(x => result.Contains(x.User) && x.DeletedAt == null, cancellationToken);
+            foreach (var order in orders)
+            {
+                if (!string.IsNullOrEmpty(order.CreatorID))
+                {
+                    string staffID = order.CreatorID;
+
+                    if (!total.ContainsKey(staffID))
+                    {
+                        total.Add(staffID, order.TotalCost);
+                    }
+                    else
+                    {
+                        total[staffID] += order.TotalCost;
+                    }
+
+                    if (!ordersCount.ContainsKey(staffID))
+                    {
+                        ordersCount.Add(staffID, 1);
+                    }
+                    else
+                    {
+                        ordersCount[staffID]++;
+                    }
+                }
+            }
+            return result.MapToStaffRevenueDtoList(_mapper, total, ordersCount);
+        }
+    }
+}
--- JewelrySalesSystem.Application/Users/UserMappingExtension.cs
+++ JewelrySalesSystem.Application/Users/UserMappingExtension.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using JewelrySalesSystem.Application.Counter;
 using JewelrySalesSystem.Application.Product;
 using JewelrySalesSystem.Application.Role;
 using JewelrySalesSystem.Domain.Entities;
@@ -29,5 +30,22 @@ namespace JewelrySalesSystem.Application.Users
             x.MapToUserDto(mapper,              
                 role.ContainsKey(x.RoleID) ? role[x.RoleID] : "Lỗi"
             )).ToList();
+
+
+        public static StaffRevenueDto MapToStaffRevenueDto(this UserEntity projectForm, IMapper mapper)
+            => mapper.Map<StaffRevenueDto>(projectForm);
+        public static StaffRevenueDto MapToStaffRevenueDto(this UserEntity projectForm, IMapper mapper, decimal totalPrice, int ordersNumber)
+        {
+            var dto = mapper.Map<StaffRevenueDto>(projectForm);
+            dto.TotalPrice = totalPrice;
+            dto.OrdersNumber = ordersNumber;
+            return dto;
+        }
+        public static List<StaffRevenueDto> MapToStaffRevenueDtoList(this IEnumerable<UserEntity> entities, IMapper mapper, Dictionary<string, decimal> totalPrice, Dictionary<string, int> ordersNumber)
+           => entities.Select(x =>
+           x.MapToStaffRevenueDto(mapper,
+               totalPrice.ContainsKey(x.ID) ? totalPrice[x.ID] : 0,
+               ordersNumber.ContainsKey(x.ID) ? ordersNumber[x.ID] : 0
+           )).ToList();
     }
 }
