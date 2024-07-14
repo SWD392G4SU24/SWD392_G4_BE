a801a7324c7fe2cf5fca8fffa9067e9408e812d5
duc-anh-le <ducanhle.iter@gmail.com>
2024-07-14 14:18:23 +0700

    Anh: Add get by date for payment method

---
+++ JewelrySalesSystem.Application/PaymentMethod/GetByDateRange/GetPaymentMethodsByDateRangeQuery.cs
@@ -0,0 +1,25 @@
+﻿using JewelrySalesSystem.Application.Common.Interfaces;
+using JewelrySalesSystem.Application.Common.Pagination;
+using MediatR;
+using System;
+
+namespace JewelrySalesSystem.Application.PaymentMethod.GetByDateRange
+{
+    public class GetPaymentMethodsByDateRangeQuery : IRequest<PagedResult<PaymentMethodDto>>, IQuery
+    {
+        public GetPaymentMethodsByDateRangeQuery() { }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public GetPaymentMethodsByDateRangeQuery(int pageNumber, int pageSize, DateTime startDate, DateTime endDate)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
--- Jewelry_Sales_System/Controllers/PaymentMethodController.cs
+++ Jewelry_Sales_System/Controllers/PaymentMethodController.cs
@@ -5,6 +5,7 @@ using JewelrySalesSystem.Application.PaymentMethod.CreatePaymentMethod;
 using JewelrySalesSystem.Application.PaymentMethod.Delete;
 using JewelrySalesSystem.Application.PaymentMethod.FilterPaymentMethod;
 using JewelrySalesSystem.Application.PaymentMethod.GetAll;
+using JewelrySalesSystem.Application.PaymentMethod.GetByDateRange;
 using JewelrySalesSystem.Application.PaymentMethod.GetById;
 using JewelrySalesSystem.Application.PaymentMethod.GetByPagination;
 using JewelrySalesSystem.Application.PaymentMethod.Update;
@@ -68,6 +69,19 @@ namespace Jewelry_Sales_System.API.Controllers
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<JsonResponse<PagedResult<PaymentMethodDto>>>> GetByFilter([FromQuery] FilterPaymentMethodQuery query, CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(query, cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpGet("payment-method/by-date-range")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(JsonResponse<PagedResult<PaymentMethodDto>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<JsonResponse<PagedResult<PaymentMethodDto>>>> GetPaymentMethodsByDateRange([FromQuery] GetPaymentMethodsByDateRangeQuery query, CancellationToken cancellationToken = default)
         {
             var result = await _mediator.Send(query, cancellationToken);
             return Ok(result);
